60bbd852410481573626cff1fe4ca90a351bf8bc
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-11-17 12:38:30 +0300

    add Interests component

---
+++ src/components/Interests.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { DefaultDeserializer } from "v8";
+
+const Interests = () => {
+    return (
+        <div>Interests</div>
+    );
+}
+
+export default Interests;
