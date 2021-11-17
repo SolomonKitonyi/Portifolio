99e8e1363897dcd7990661888fd89d419a5d441a
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-11-17 13:28:50 +0300

    add changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,7 +11,7 @@ const Main = () => {
             </div>
             <h2 style={{textAlign: "center"}}>STACKS</h2>
             <div className={Classes.stack}>
-                <Stack name="React" background="grey" logo="images/logo512.png"/>
+                {<Stack name="React" background="grey" logo="images/logo512.png"/> }
                 <Stack name="NodeJS" background="light-grey" logo="images/node.png"/>
                 <Stack name="MongoDB" background="grey" logo="images/mongo.png"/>
                 <Stack name="Express" background="grey" logo="images/express.png"/>
