d8de0206f793b919aa3713fb5d4fd54f17c5f19a
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-11-18 13:02:35 +0300

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,16 @@ import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Profile from './components/Profile';
+import Stacks from './components/Stacks';
+import Interests from './components/Interests';
 
 ReactDOM.render(
   <Router>
     <Routes>
-        <Route path="/profile" element={<Profile />}/>
         <Route path="/" element={<App />}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/stacks" element={<Stacks/>}/>
+        <Route path="/interests" element={<Interests/>}/>
       </Routes>
   </Router>,
   document.getElementById('root')
