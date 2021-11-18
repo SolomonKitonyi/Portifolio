89e7c4a6c397c5109d06d59105e12b2738c1d44e
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-11-18 13:02:21 +0300

    add header

--- src/components/Stacks.tsx
+++ src/components/Stacks.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "./Header";
 
 const Stacks = () => {
     return (
-        <div>Stacks</div>
+        <div>
+            <Header />
+        </div>
     );
 }
 
